edb53c9e9dff92a26653c51394bca182cc1f1346
Julio Eliecer Nova Estrada <142542719+NovaJulio@users.noreply.github.com>
2024-06-05 16:40:11 -0500

    AddElement carrito sirve

--- src/main/java/com/mycompany/tranbajofinalprogii/SecondaryController.java
+++ src/main/java/com/mycompany/tranbajofinalprogii/SecondaryController.java
@@ -3,9 +3,15 @@ package com.mycompany.tranbajofinalprogii;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class SecondaryController {
 
@@ -17,6 +23,8 @@ public class SecondaryController {
     AnchorPane carritoTab = new AnchorPane();
     @FXML
     Button button = new Button();
+    @FXML
+    AnchorPane rowAdd = new AnchorPane();
 
     public void cerra() {
         System.exit(0);
@@ -30,14 +38,51 @@ public class SecondaryController {
         }
     }
 
-    public void addElement() {
-        carritoView.setPrefHeight(carritoHeight()+200);
+    public AnchorPane rowadd() {
         AnchorPane rowadd = new AnchorPane();
-        rowadd.setPrefSize(Double.MAX_VALUE, 200);
-        rowadd.getStylesheets().add("file:/"+System.getProperty("user.dir").replace("\\", "/")+ "/target/classes/com/mycompany/tranbajofinalprogii/StyleGradient.css");
-        rowadd.getStyleClass().add("carritoLabel");
-        carritoView.add(rowadd, 0, i);
+        Label lbl = new Label();
+        Button btt = new Button();
         
+        Image img = new Image("file:src/main/resources/icons/imgNotFound.jpg");
+        ImageView iv = new ImageView(img);
+        iv.setFitHeight(150);
+        iv.setFitWidth(162);
+        iv.setLayoutX(66);
+        iv.setLayoutY(25);
+        iv.preserveRatioProperty().set(true);
+        lbl.setPrefSize(330, 89);
+        lbl.setLayoutX(253);
+        lbl.setLayoutY(56);
+        lbl.setFont(Font.font("System", FontWeight.BOLD, 24));
+        lbl.setText("LoremIpsum");
+        lbl.setTextFill(Color.BLACK);
+        btt.setPrefSize(108, 43);
+        btt.setLayoutX(557);
+        btt.setLayoutY(145);
+        btt.setFont(Font.font("System", FontWeight.BOLD, 12));
+        btt.setText("Eliminar");
+        btt.setGraphicTextGap(10);
+        Image trashicon = new Image("file:src/main/resources/icons/trash.png");
+        ImageView a = new ImageView(trashicon);
+        a.setFitHeight(20);
+        a.setFitWidth(20);
+        btt.setGraphic(a);
+        btt.setStyle("-fx-background-radius: 15;-fx-border-radius: 15;");
+
+        rowadd.setPrefSize(Double.MAX_VALUE, 200);
+        rowadd.getStylesheets().add("file:/" + System.getProperty("user.dir"
+                + "").replace("\\", "/") + "/target/classes/com/mycompany/tranbajofinalprogii/StyleGradient.css");
+        rowadd.getStyleClass().add("carritoLabel");
+        rowadd.getChildren().add(lbl);
+        rowadd.getChildren().add(btt);
+        rowadd.getChildren().add(iv);
+        return rowadd;
+    }
+
+    public void addElement() {
+        carritoView.setPrefHeight(carritoHeight() + 200);
+        carritoView.add(rowadd(), 0, i);
+
         i++;
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.tranbajofinalprogii {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.tranbajofinalprogii to javafx.fxml;
     exports com.mycompany.tranbajofinalprogii;
