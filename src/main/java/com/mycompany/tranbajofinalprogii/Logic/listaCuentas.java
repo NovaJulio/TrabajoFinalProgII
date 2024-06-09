0048379b7eb4bc9fd0874df0c9f91691c0dcb8fb
Julio Eliecer Nova Estrada <142542719+NovaJulio@users.noreply.github.com>
2024-06-09 01:38:57 -0500

    CreateCuenta Añadido

--- src/main/java/com/mycompany/tranbajofinalprogii/Logic/listaCuentas.java
+++ src/main/java/com/mycompany/tranbajofinalprogii/Logic/listaCuentas.java
@@ -11,10 +11,24 @@ public class listaCuentas {
         cab = null;
     }
 
+    // Returna el ultimo elemento
+    public cuenta getEnd() {
+        cuenta j = cab;
+        if (isEmpty()) {
+            return null;
+        } else {
+            j = cab.prev;
+            return j;
+        }
+    }
+
+    // Comprueba si la lista esta vacia
     boolean isEmpty() {
         return cab == null;
     }
 
+    // Busca una cuenta que tenga el nombre de usuario dado, si no la encuentra,
+    // returna null
     public cuenta searchUser(String User) {
         if (isEmpty()) {
             return null;
@@ -31,7 +45,30 @@ public class listaCuentas {
         }
     }
 
+    // Crea la cuenta y la añade al final de la lista
     void createCuenta(TextField user, PasswordField pass) {
-
+        if (searchUser(user.getText()) == null) {
+            cuenta info = new cuenta(user.getText(), pass.getText());
+            cuenta ult = getEnd();
+            if (cab == null) {
+                cab = info;
+                cab.next = cab;
+                cab.prev = cab;
+            } else if (ult == cab) {
+                cab.next = info;
+                cab.prev = info;
+                info.next = cab;
+                info.prev = cab;
+            } else {
+                ult.next = info;
+                info.prev = ult;
+                info.next = cab;
+                cab.prev = info;
+            }
+        } else {
+            System.out.println("Usuario existente");
+        }
     }
+
+    //
 }
